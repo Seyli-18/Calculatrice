529161010f8281168db87104df8bed529075bfaf
Seyli-18 <ilyessbia4@gmail.com>
2025-06-24 14:17:02 +0200

    Create jeux.js

---
+++ jeux.js
@@ -0,0 +1,4 @@
+// Tu peux ajouter ici des interactions pour la page principale des jeux.
+// Exemple : affichage dynamique de nouveaux jeux, animation, etc.
+
+console.log("Page des jeux chargée !");
