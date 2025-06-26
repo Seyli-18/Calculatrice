73cdb48ea56e86ec445a30d25296b5ced56a69b2
Seyli-18 <ilyessbia4@gmail.com>
2025-06-26 15:55:38 +0200

    Create brick.js

---
+++ brick.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "white";
+  ctx.font = "20px Arial";
+  ctx.fillText("Casse Brique en développement...", 40, 160);
+};
